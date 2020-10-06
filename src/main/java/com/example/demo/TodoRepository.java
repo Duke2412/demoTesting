d23db41c90c37653f45fb1352d65000e2df8cd85
Duke2412 <66355961+Duke2412@users.noreply.github.com>
2020-10-06 17:08:38 +0700

    Up test lên

---
+++ src/main/java/com/example/demo/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface TodoRepository {
+    List<Todo> findAll();
+
+    Todo findById(int id);
+}
